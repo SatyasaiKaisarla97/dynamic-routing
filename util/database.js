128606b161027427d2eb924ff41f4df9fe7adb58
Satyasai <satyasai1997@gmail.com>
2023-12-18 14:00:02 +0530

    sql-op

--- util/database.js
+++ util/database.js
@@ -1,9 +1,8 @@
-const { Sequelize } = require("sequelize");
+const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize("node-complete", "root", "Satyasaik123", {
-  dialect: "mysql",
-  host: "localhost",
+const sequelize = new Sequelize('node-complete', 'root', 'Satyasaik123', {
+  dialect: 'mysql',
+  host: 'localhost'
 });
-
 
 module.exports = sequelize;
